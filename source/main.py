ce463c6cd6243b3d6c98bd87aebc07d70bb4aad0
Andrew <andrew.lipson737@gmail.com>
2022-01-04 16:33:07 +1100

    asking for user input with error handling

--- source/main.py
+++ source/main.py
@@ -1,1 +1,11 @@
+def user_input():
+    while True:
+        try:
+            val = int(input("Who won the point: "))
+            if val == 1 or val == 2:
+                return val
+            print("Please enter either '1' or '2'. ")
+        except ValueError:
+            print("Please enter either '1' or '2'. ")
 
+print(user_input())
